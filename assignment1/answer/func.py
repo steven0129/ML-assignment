80d40cdfbf3f791a91c0566b868f0496bffb5bec
steven0129 <h3584935@gmail.com>
2018-03-30 22:43:56 +0800

    資料讀取完成

---
+++ assignment1/answer/func.py
@@ -0,0 +1,30 @@
+import numpy as np
+import os
+
+
+def SAD(img1, img2):
+    x = np.array(img1)
+    y = np.array(img2)
+
+    distance = lambda a, b: a.astype(float)-b.astype(float)
+    return np.sum(abs(distance(x, y)))
+
+
+def SSD(img1, img2):
+    x = np.array(img1)
+    y = np.array(img2)
+
+    distance = lambda a, b: a.astype(float)-b.astype(float)
+    return np.sum(abs(distance(x, y))**2)
+
+
+def dataset():
+    dataset = []
+
+    for myDir in os.listdir('CroppedYale'):
+        files = os.listdir('CroppedYale/{}'.format(myDir))
+
+        for myFile in files:
+            dataset.append((myDir, myFile))
+
+    return dataset
---
+++ assignment1/answer/main.py
@@ -0,0 +1,6 @@
+from skimage import io
+import numpy as np
+import func
+
+for data in func.dataset():
+    print(data)
