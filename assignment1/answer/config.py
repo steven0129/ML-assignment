4fb7be55f080811e7390c6546888ee265f33623c
steven0129 <h3584935@gmail.com>
2018-04-03 11:02:20 +0800

    加入python-fire

---
+++ assignment1/answer/config.py
@@ -0,0 +1,3 @@
+class Env(object):
+    k=1 # KNN分類演算法的k值
+    distance_type='ssd' # KNN分類演算法計算距離的公式
--- assignment1/answer/main.py
+++ assignment1/answer/main.py
@@ -1,23 +1,33 @@
 from skimage import io
 from tqdm import tqdm
+from config import Env
 import numpy as np
 import func
 import os
+import fire
 
-dataset = list(func.dataset())
-trainingSet = []
-testingSet = []
+options = Env()
 
-for label in os.listdir('CroppedYale'):
-    data = list(filter(lambda x: x[0]==label, dataset))
-    (trainData, testData) = (data[:35], data[35:])
-    trainingSet.extend(trainData)
-    testingSet.extend(testData)
+def train(**kwargs):
+    dataset = list(func.dataset())
+    trainingSet = []
+    testingSet = []
 
-correct = count = 0
-for testData in tqdm(testingSet):
-    (label, data) = testData
-    result = func.kNearestNeighbor(trainingSet, testData, k=1, distance_type='ssd')
-    correct = correct + 1 if label==result else correct
-    count = count + 1
-    tqdm.write('正確分類: {0}/{1}, 準確率: {2}%'.format(correct, count, float(correct)/float(count)*100))
+    for label in os.listdir('CroppedYale'):
+        data = list(filter(lambda x: x[0] == label, dataset))
+        (trainData, testData) = (data[:35], data[35:])
+        trainingSet.extend(trainData)
+        testingSet.extend(testData)
+
+    correct = count = 0
+    for testData in tqdm(testingSet):
+        (label, data) = testData
+        result = func.kNearestNeighbor(
+            trainingSet, testData, k=options.k, distance_type=options.distance_type)
+        correct = correct + 1 if label == result else correct
+        count = count + 1
+        tqdm.write('正確分類: {0}/{1}, 準確率: {2}%'.format(correct,
+                                                    count, float(correct)/float(count)*100))
+
+if __name__=='__main__':
+    fire.Fire()
