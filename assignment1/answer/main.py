dfebe394c188416263e622b1bf0c96e6f367e1bb
steven0129 <h3584935@gmail.com>
2018-04-03 22:04:47 +0800

    Add hyperparameter optimization

--- assignment1/answer/main.py
+++ assignment1/answer/main.py
@@ -1,6 +1,7 @@
 from skimage import io
 from tqdm import tqdm
 from config import Env
+from random import *
 import numpy as np
 import func
 import os
@@ -9,7 +10,7 @@ import fire
 options = Env()
 
 
-def train(**kwargs):
+def trainingProcess(k=1, distance_type='ssd'):
     dataset = list(func.dataset())
     trainingSet = []
     testingSet = []
@@ -22,16 +23,40 @@ def train(**kwargs):
 
     correct = count = 0
     for testData in tqdm(testingSet):
-        label, data = testData
+        label, _ = testData
         result = func.kNearestNeighbor(
-            trainingSet, testData, k=options.k,
-            distance_type=options.distance_type)
+            trainingSet, testData, k=k,
+            distance_type=distance_type)
 
         correct = correct + 1 if label == result else correct
         count = count + 1
         tqdm.write('正確分類: {0}/{1}, 準確率: {2}%'.format(correct,
                                                      count, float(correct)/float(count)*100))
 
+    return correct, count, float(correct)/float(count)*100
+
+
+def train():
+    correct, count, acc = trainingProcess(k=options.k, distance_type=options.distance_type)
+    print('正確分類: {0}/{1}, 準確率: {2}%'.format(correct, count, acc))
+
+
+def hyperparameter():
+    with open('hyperparameter.csv', 'a') as f:
+        result = []
+        total_distance_type = ['sad', 'ssd']
+        for i in tqdm(range(230)):
+            randomK = randint(1, 500)
+            randomD = total_distance_type[randint(0, 1)]
+            tqdm.write('開始Sample第{0}次 (k={1}, randomD={2})'.format(i + 1, randomK, randomD))
+            _, _, acc = trainingProcess(k=randomK, distance_type=randomD)
+            result.append({'acc': acc, 'k': randomK, 'distance_type': randomD})
+            f.write(
+                '\n{0}, {1}, {2}'.format(randomK,randomD,acc))
+
+        maxObj = max(result, key=lambda x: x['acc'])
+        print('最高準確率: {0} (k={1}, distance_type={2})'.format(
+            maxObj['acc'], maxObj['k'], maxObj['distance_type']))
 
 if __name__ == '__main__':
     fire.Fire()
