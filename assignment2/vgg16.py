b9c64362cd24846cac3e98810876c895136e8237
steven0129 <h3584935@gmail.com>
2018-05-06 16:57:10 +0800

    Framework complete

--- assignment2/vgg16.py
+++ assignment2/vgg16.py
@@ -1,38 +1,17 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-
-import tensorflow as tf
-import keras.backend.tensorflow_backend as KTF
-import os
-import warnings
-import os, sys
-import numpy as np
-import keras
-from skimage import io,transform
-from tflearn.layers.conv import global_avg_pool
+from keras.models import Sequential
 from keras.models import Model
 from keras.layers import Input, Dense, Activation, Flatten, Conv2D, MaxPooling2D, AveragePooling2D
 from keras.layers import GlobalAveragePooling2D, GlobalMaxPooling2D, BatchNormalization
-from keras.models import Model
-from keras import layers
-from keras import backend as K
-#进行配置，使用40%的GPU
-config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.4
-session = tf.Session(config=config)
-# 设置session
-KTF.set_session(session )
+from keras.optimizers import SGD
+from skimage import io
+from sklearn.preprocessing import OneHotEncoder
+import tensorflow as tf
+import keras.backend.tensorflow_backend as KTF
+import keras
+import numpy as np
+import cv2
 
-
-
-# In[ ]:
-
-
-def VGG16(input_tensor=None, input_shape=None):
-
+def VGG_16(classify_num, weights_path=None, input_shape=[224, 224, 3]):
     img_input = Input(shape=input_shape)
 
     # Block 1
@@ -68,26 +47,30 @@ def VGG16(input_tensor=None, input_shape=None):
     x = Flatten(name='flatten')(x)
     x = Dense(4096, activation='relu', name='fca')(x)
     x = Dense(4096, activation='relu', name='fcb')(x)
-    x = Dense(12, activation='softmax', name='Classification')(x)
+    x = Dense(classify_num, activation='softmax', name='Classification')(x)
 
 
     inputs = img_input
     # Create model.
-    model = Model(inputs, x, name='vgg16')
+    model = Model(inputs=inputs, outputs=x, name='vgg16')
+
+    if weights_path:
+        model.load_weights(weights_path)
 
     return model
 
+if __name__ == "__main__":
+    img1 = cv2.resize(cv2.imread('1.pgm'), (224, 224)).astype(np.float32).tolist()
+    img2 = cv2.resize(cv2.imread('2.pgm'), (224, 224)).astype(np.float32).tolist()
+    data = np.array([img1, img2])
 
-# In[ ]:
+    encoder = OneHotEncoder()
+    labels = encoder.fit_transform([[1], [2]]).toarray()
 
-
-model = VGG16(input_shape=[224, 224, 3])
-model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adam(lr=1e-4), metrics=['accuracy'])
-model.load_weights("model.h5")
-
-
-# In[ ]:
-
-
-get_ipython().system('jupyter nbconvert --to script vgg16.ipynb')
-
+    model = VGG_16(classify_num=labels.shape[0])
+    model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adam(lr=1e-4), metrics=['accuracy'])
+    model.fit(x=data, y=labels, epochs=1)
+    
+    out = model.predict(np.array([img1, img2]))
+    print(out)
+    print(np.argmax(out, axis=1))
